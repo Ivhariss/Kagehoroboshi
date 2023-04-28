b9eeb4fc0bbec4b90c6bc624239fd998c8908d04
slugrain <ikishi1106@gmail.com>
2023-04-28 18:20:45 +0900

    HPバー仮置き。スクリプト全然組んでない。

---
+++ Assets/Ayumu_Hayashi/Scripts/HPBarScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//  UIg‚¤‚Æ‚«‚Í‚±‚ê“ü‚ê‚é‚Æ‚¢‚¢‚ç‚µ‚¢A‚Æ‚¢‚¤‚ ‚â‚Ó‚â‚ÈŠ´‚¶‚Å“ü‚Á‚Ä‚é
+using UnityEngine.UI;
+
+public class HPBarScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
