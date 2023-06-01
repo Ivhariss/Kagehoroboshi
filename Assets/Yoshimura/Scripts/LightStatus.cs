4ce10a60d1cd74ec1ffeee52da14e64028fddcf3
DESKTOP-8CRU2DJ\PC_User <show20010521@gmail.com>
2023-06-01 12:34:42 +0900

    統合中

--- Assets/Yoshimura/Scripts/LightStatus.cs
+++ Assets/Yoshimura/Scripts/LightStatus.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public class LightStatus : MonoBehaviour
 {
+    [SerializeField] Item item;
+    
     public static LightStatus instance;
 
-    [SerializeField]
-   public float LightHP = 0; //アイテムの耐久値
-
-    private float flashTime;
+    public float LightHP; //アイテムの耐久値
 
     //Objectについてるlight
     public GameObject flashLight;
@@ -25,7 +26,6 @@ public class LightStatus : MonoBehaviour
         flashItem();
      }
 
-
   public void flashItem()
     {
         if(LightHP >= 1) 
@@ -35,12 +35,24 @@ public class LightStatus : MonoBehaviour
                
         }
 
-
         if (1 >= LightHP)
         {
             flashLight.SetActive(false);
         }
 
+    }
+
+     void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("aaa");
+
+        ItemBox.instance.SetItem(item);
+
+        if (Input.GetKeyUp(KeyCode.Space)&&collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("www");
+           
+        }
     }
 
 
--- Assets/Yoshimura/Scripts/PoketItem.cs
+++ Assets/Yoshimura/Scripts/PoketItem.cs
@@ -6,6 +6,19 @@ using System;
 [Serializable]
 public class PoketItem
 {
-    public GameObject item;
-    
+    public Type type; // Ží—Þ
+    public Sprite sprite; // ‰æ‘œ(’Ç‰Á)
+    public GameObject Item;
+
+    public enum Type
+    {
+        LightItem,
+    }
+
+    public PoketItem(PoketItem item)
+    {
+        this.type = item.type;
+        this.sprite = item.sprite; // ‰æ‘œ(’Ç‰Á)
+        this.Item = item.Item;
+    }
 }
