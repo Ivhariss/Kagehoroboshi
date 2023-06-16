35c7157a5c34af68b39a8f8dde2bf49288913f9a
田中 <tana.di0211@gmail.com>
2023-06-16 17:52:34 +0900

    Text

---
+++ Assets/tanaka/Scripts/Text.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+    private void OncollisionEnter3D(Collider collision)
+    {
+        if (collision.gameObject.name == "Enemy")
+        {
+
+        }
+    }
+}
